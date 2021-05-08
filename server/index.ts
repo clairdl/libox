bab563dfc4d9f97dda94a96fcea1ab1d19c2a1ae
clairdl <52753311+clairdl@users.noreply.github.com>
2021-05-09 00:05:39 +0100

    bizarre line; breaks and fixes everything

--- server/index.ts
+++ server/index.ts
@@ -12,6 +12,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// app.set('query parser', 'simple');
 
 // Routes
 app.use('/', getRoutes);
