a2837eb2138c21d1105022e9320aa93fed7a3c73
clairdl <52753311+clairdl@users.noreply.github.com>
2021-05-11 11:49:04 +0100

    hooks > class components

--- client/src/components/Nav/NavbarSearch.js
+++ client/src/components/Nav/NavbarSearch.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Search } from 'grommet-icons';
 import { Box, TextInput } from 'grommet';
 
-const NavSearchbar = () => (
-  <Box width="medium" gap="medium">
-    <TextInput icon={<Search />} placeholder="search ..." />
-  </Box>
-);
+const NavbarSearch = () => {
+  const [query, setQuery] = useState('');
 
-export default NavSearchbar;
+  return (
+    <Box width="medium" gap="medium">
+      <TextInput
+        value={query}
+        onChange={({ target }) => {
+          setQuery(target.value);
+        }}
+        icon={<Search />}
+        placeholder="search ..."
+      />
+    </Box>
+  );
+};
+
+export default NavbarSearch;
