092bb352cb915aba9edce6fdcc90749a703b2120
clairdl <52753311+clairdl@users.noreply.github.com>
2021-05-11 11:48:38 +0100

    remove useless class component

--- client/src/components/Nav/Navbar.js
+++ client/src/components/Nav/Navbar.js
@@ -5,16 +5,28 @@ import { Nav, Anchor, Header } from 'grommet';
 
 import NavbarSearch from './NavbarSearch';
 
-export default class Navbar extends React.Component {
-  render() {
-    return (
-      <Header background="brand" justify="left">
-        <Nav direction="row" pad="medium" align="center">
-          <Anchor as={Link} to="/" label="home" />
-          <Anchor as={Link} to="/watchlist" label="watchlist" />
-        </Nav>
-        <NavbarSearch />
-      </Header>
-    );
-  }
-}
+const Navbar = () => (
+  <Header background="brand" justify="left">
+    <Nav direction="row" pad="medium" align="center">
+      <Anchor as={Link} to="/" label="home" />
+      <Anchor as={Link} to="/watchlist" label="watchlist" />
+    </Nav>
+    <NavbarSearch />
+  </Header>
+);
+
+export default Navbar;
+
+// export default class Navbar extends React.Component {
+//   render() {
+//     return (
+//       <Header background="brand" justify="left">
+//         <Nav direction="row" pad="medium" align="center">
+//           <Anchor as={Link} to="/" label="home" />
+//           <Anchor as={Link} to="/watchlist" label="watchlist" />
+//         </Nav>
+//         <NavbarSearch />
+//       </Header>
+//     );
+//   }
+// }
