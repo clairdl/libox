f8a9947f8a529f35aafa7504cfc04fa0abe8e520
clairdl <52753311+clairdl@users.noreply.github.com>
2021-05-12 23:17:50 +0100

    search implemented

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
+import NoResults from '../components/Search/NoResults';
 
 const Search = () => {
-  const [data, setData] = useState({ rows: [] });
+  const [data, setData] = useState({ rows: [], totalResults: null });
   const { search } = useLocation();
 
   // Every time the search query updates, we fetch again
@@ -11,17 +12,22 @@ const Search = () => {
     const fetchData = async () => {
       console.log('fetchData invoked');
       const res = await axios.get(`/search${search}`);
-      setData({ rows: res.data.results });
+      setData({
+        rows: res.data.results,
+        totalResults: res.data.total_results,
+      });
     };
     fetchData();
   }, [search]);
 
+  const listItems = data.rows.map((e) => <li key={e.id}>{e.title}</li>);
+  const noResults = data.totalResults === 0 ? <NoResults /> : null;
+
   return (
-    <ul>
-      {data.rows.map((item) => (
-        <li key={item.id}>{item.title}</li>
-      ))}
-    </ul>
+    <div>
+      {noResults}
+      <ul>{listItems}</ul>
+    </div>
   );
 };
 
