416a7da67e5188081b2ebf6f28e22b57a4c3fb01
clairdl <52753311+clairdl@users.noreply.github.com>
2021-05-13 17:23:47 +0100

    redux search action/reducers implemented

---
+++ client/src/reducers/searchReducer.js
@@ -0,0 +1,19 @@
+import * as actions from '../actions/types';
+
+const initialState = {
+  searchData: {},
+};
+
+
+// 
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case actions.SEARCH_API:
+      return {
+        ...state,
+        searchData: action.payload,
+      };
+    default:
+      return state;
+  }
+}
