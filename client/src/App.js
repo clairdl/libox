08b5d7c51a720d6ecf5c532d2df4df30b46a084e
clairdl <52753311+clairdl@users.noreply.github.com>
2021-05-16 19:31:12 +0100

    refactor from local state to global redux state

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import { Grommet } from 'grommet';
-
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+
+import { Grommet } from 'grommet';
 import Navbar from './components/Nav/Navbar';
 import Homep from './pages/Home';
 import Watchlist from './pages/Watchlist';
--- client/src/components/Nav/NavbarSearch.js
+++ client/src/components/Nav/NavbarSearch.js
@@ -1,33 +1,41 @@
-import React, { useState } from 'react';
+// libs
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
+// components
 import { Search } from 'grommet-icons';
 import { Box, TextInput } from 'grommet';
 
+// redux
+import { updateQuery, selectQuery, getMovies } from '../../slices/searchSlice';
+
 const NavbarSearch = () => {
+  // useDispatch returns a reference to the dispatch function from the Redux store
+  const dispatch = useDispatch();
+  // useHistory hook returns the history instance
   const history = useHistory();
 
-  const [query, setQuery] = useState('');
-  const handleChange = ({ target }) => {
-    setQuery(target.value);
-  };
+  // query source of truth
+  const query = useSelector(selectQuery);
 
   const handleSubmit = ({ code }) => {
-    // If enter key is pressed
     if (code === 'Enter') {
-      // Update URL route, which renders the Search page
+      // update URL route, thus rendering the 'Search' page
       history.push(`/search?query=${query}`);
+      // dispatch thunk, updating redux with search results
+      dispatch(getMovies());
     }
   };
 
   return (
-    <Box width="medium" gap="medium">
+    <Box width='medium' gap='medium'>
       <TextInput
         value={query}
         onKeyDown={handleSubmit}
-        onChange={handleChange}
+        onChange={(e) => dispatch(updateQuery(e.target.value))}
         icon={<Search />}
-        placeholder="search ..."
+        placeholder='search ...'
       />
     </Box>
   );
