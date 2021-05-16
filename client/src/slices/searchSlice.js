360d4b98f226cdc8908b93bc5515db3ef13ced54
clairdl <52753311+clairdl@users.noreply.github.com>
2021-05-16 19:35:14 +0100

    implement fetching movies from tmdb api

--- client/src/slices/searchSlice.js
+++ client/src/slices/searchSlice.js
@@ -1,9 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
-  query: ''
+  query: '',
+  status: 'idle',
+  movies: [],
+  totalResults: null,
 };
 
+// Thunks
+export const getMovies = createAsyncThunk(
+  'search/getMovies',
+  async (payload, store) => {
+    try {
+      const res = await axios.get(
+        `/search?query=${store.getState().search.query}`
+      );
+      return res.data;
+    } catch (err) {
+      return err;
+    }
+  }
+);
+
+// Create slice
 export const searchSlice = createSlice({
   name: 'search',
   initialState,
@@ -12,8 +32,24 @@ export const searchSlice = createSlice({
       state.query = action.payload;
     },
   },
+  extraReducers: {
+    [getMovies.fulfilled]: (state, action) => {
+      // if fulfilled, update state
+      state.movies = action.payload.results;
+      state.totalResults = action.payload.total_results;
+    },
+  },
 });
 
+// Actions
 export const { updateQuery } = searchSlice.actions;
 
+// Reducers
 export default searchSlice.reducer;
+
+// Selectors
+export const selectQuery = (state) => state.search.query;
+export const selectAllMovies = (state) => state.search.movies;
+export const selectTotalResults = (state) => state.search.totalResults;
+export const selectMovieById = (state, id) =>
+  state.search.movies.find((el) => el.id === id);
