51e74753165058f8fb8981bade0c08022029f169
clairdl <52753311+clairdl@users.noreply.github.com>
2021-05-19 22:13:42 +0100

    TS refactor redux store config

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 // REDUX
 import { Provider } from 'react-redux';
-import store from './store';
+import { store } from './store';
 
 import './index.css';
 import App from './App';
