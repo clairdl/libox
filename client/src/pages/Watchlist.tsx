9ab9e56cb1d910b056969655912bd9de53af9faa
clairdl <52753311+clairdl@users.noreply.github.com>
2021-05-20 00:55:24 +0100

    file renames

--- client/src/pages/Watchlist.tsx
+++ client/src/pages/Watchlist.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import WatchlistTable from '../components/Watchlist/Table';
 
 export default function Watchlist() {
   return (
     <div>
       <h1>Watchlist</h1>
+      <WatchlistTable />
     </div>
   );
 }
