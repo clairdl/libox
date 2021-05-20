8b8c7902312c1967aa3f18b0f4367b5e492efb6b
clairdl <52753311+clairdl@users.noreply.github.com>
2021-05-20 18:43:39 +0100

    implemented AddWatchlistBtn

--- client/src/components/generic/AddWatchlistBtn.tsx
+++ client/src/components/generic/AddWatchlistBtn.tsx
@@ -1,16 +1,32 @@
+// core libs
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
+// components
 import { Button } from 'grommet';
 import { Add } from 'grommet-icons';
+
+// redux
+import { getMovieDetails } from '../../slices/list/listSlice';
 
 type AddWatchlistBtnProps = {
   id: number;
 };
+
 export const AddWatchlistBtn = (props: AddWatchlistBtnProps) => {
-  
-  // const onClickHandler = () => {};
+  const dispatch = useDispatch();
+
+  const onClickHandler = () => {
+    dispatch(getMovieDetails({ listId: 'watchlist', id: props.id }));
+  };
 
   return (
-    <Button icon={<Add />} size='small' label='watchlist' onClick={() => {}} />
+    <Button
+      icon={<Add />}
+      size='small'
+      label='watchlist'
+      onClick={onClickHandler}
+    />
   );
 };
 
