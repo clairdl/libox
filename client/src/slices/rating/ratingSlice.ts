e436d2188c97319e10286f4458fd84f3f7fe5b87
clairdl <52753311+clairdl@users.noreply.github.com>
2021-05-23 23:30:09 +0100

    add delete rating action

--- client/src/slices/rating/ratingSlice.ts
+++ client/src/slices/rating/ratingSlice.ts
@@ -1,12 +1,12 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '../../store';
-import { CreateOrUpdateRatingPayload, InitialState } from './types';
+import { InitialState, CreateOrUpdateRatingPayload, MovieId, MovieRating } from './types';
 
-const initialState = {} as InitialState
-
-// export interface InitialState {
-//   ratings: Record<MovieId, MovieRating>
-// }
+const initialState: InitialState = {
+  idToRatingMap: {
+    65976: 0
+  }
+}
 
 export const ratingSlice = createSlice({
   name: 'rating',
@@ -15,18 +15,45 @@ export const ratingSlice = createSlice({
     createOrUpdateRating: (state, action: PayloadAction<CreateOrUpdateRatingPayload>): void => {
       const { id, rating } = action.payload;
 
-      if (state.ratings[id] !== undefined) {
-        state.ratings[id] = rating
-      } else {
-        Object.assign(state.ratings, { [id]: rating })
-      }
+      state.idToRatingMap[id] = rating
+      // state.idToRatingMap[id] = rating;
+      // // Create
+      // if (state.idToRatingMap[id] !== undefined) {
+      //   state.idToRatingMap[id] = rating
+      //   // Update
+      // } else {
+      //   state.idToRatingMap[id] = rating
+      //   // Object.assign(state.idToRatingMap, { [id]: rating })
+      // }
     },
+    deleteRating: (state, action: PayloadAction<MovieId>): void => {
+      delete state.idToRatingMap[action.payload];
+    }
   }
+});
 
-})
+// Actions
+export const { createOrUpdateRating } = ratingSlice.actions;
+export const { deleteRating } = ratingSlice.actions;
 
 // Reducer
 export default ratingSlice.reducer;
 
 // Selectors
-export const selectRatingById = (state: RootState, id: number) => state.rating[id];
+export const selectEntireRatingMap = (state: RootState): Record<MovieId, MovieRating> | undefined => state.rating.idToRatingMap
+
+
+export const selectRatingById = (state: RootState, id: number): number | null => {
+  // if the movie has a rating, return its rating, otherwise return null
+  return state.rating.idToRatingMap[id] || null;
+}
+
+/*
+
+1. render rating button
+  - needs the movie id and the rating (if there is one)
+
+
+
+
+*/
