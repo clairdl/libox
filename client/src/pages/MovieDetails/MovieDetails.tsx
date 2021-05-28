b75572447fef527ee667729bd1a7778a88964121
clairdl <52753311+clairdl@users.noreply.github.com>
2021-05-28 17:51:20 +0100

    movie details page implemented v1

--- client/src/pages/MovieDetails/MovieDetails.tsx
+++ client/src/pages/MovieDetails/MovieDetails.tsx
@@ -1,91 +1,152 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useLayoutEffect } from 'react';
 import axios from 'axios';
-import { Box, Button, Grid, Image, Stack } from 'grommet';
-import { MovieDetailsDataResponse } from './types';
-import HeaderButtons from './HeaderButtons';
+import {
+  Box,
+  Button,
+  Grid,
+  Heading,
+  Image,
+  Meter,
+  Stack,
+  Text,
+  Tabs,
+  Tab,
+} from 'grommet';
+import AddToListButton from '../../components/generic/AddToListButton';
+import Spinner from '../../components/generic/Spinner';
+import { MovieDetailsData, MovieCreditsData, Crew } from './types';
+import HeaderButtons from './NavButtons';
 
 import { LinkPrevious } from 'grommet-icons';
 import FastAverageColor from 'fast-average-color';
+import BackdropImage from './BackdropImage';
+import TitleText from './TitleText';
+import RatingBtn from '../../components/generic/RatingBtn';
+import RatingPanel from './RatingPanel';
+import CastCrewDetailsTabs from './CastCrewDetailsTabs';
 
 interface Props {
   id: number;
 }
 
 const MovieDetails = (props: Props) => {
-  const [mv, setMovieData] = useState({} as MovieDetailsDataResponse);
-  const [col, setCol] = useState('rgba(255,255,255,1)');
+  const [isLoading, setIsLoading] = useState(true);
+  const [mvD, setMovieDetails] = useState({} as MovieDetailsData);
+  const [mvC, setMovieCredits] = useState({} as MovieCreditsData);
 
+  const [dir, setDir] = useState('');
+  const [col, setCol] = useState('rgba(0,0,0,0)');
+
+  // data fetching & refining
   useEffect(() => {
-    console.log(1);
     const fetchData = async () => {
-      const result = await axios(`/movie/${props.id}`);
-      setMovieData(result.data);
+      const resDetails = await axios(`/movie/${props.id}`);
+      const resCredits = await axios(`/movie/${props.id}/credits`);
+      setMovieDetails(resDetails.data);
+      setMovieCredits(() => {
+        setDir(
+          resCredits.data.crew.find((el: any) => el.job === 'Director').name
+        );
+        return resCredits.data;
+      });
     };
     fetchData();
   }, []);
 
+  // get colour for backdrop
   useEffect(() => {
-    console.log(2, mv);
     const fac = new FastAverageColor();
     fac
-      .getColorAsync(`https://www.themoviedb.org/t/p/w300/${mv.backdrop_path}`)
-      .then((color) => {
-        console.log(color);
-        setCol(color.rgba);
-        // {
-        //     rgb: 'rgb(255, 0, 0)',
-        //     rgba: 'rgba(255, 0, 0, 1)',
-        //     hex: '#ff0000',
-        //     hexa: '#ff0000ff',
-        //     value: [255, 0, 0, 255],
-        //     isDark: true,
-        //     isLight: false
-        // }
+      .getColorAsync(`https://www.themoviedb.org/t/p/w300/${mvD.backdrop_path}`)
+      .then((res) => {
+        setCol(res.rgba);
+        setIsLoading(false);
       })
       .catch((e) => {
         console.error('fast-average-color err: ', e);
       });
-
     return () => {
       fac.destroy();
     };
-  }, [mv]);
-  console.log(col);
+  }, [mvD]);
+
+  let details = [
+    { name: 'runtime', val: `${mvD.runtime}mins` },
+    { name: 'revenue', val: `${mvD.revenue} USD` },
+    { name: 'budget', val: `${mvD.budget} USD` },
+  ];
 
   return (
-    <Box background={col}>
-      <Button secondary alignSelf='start' icon={<LinkPrevious />} />
-      <Box background={col}>
-        
-        <Box height='xlarge' width='xlarge' alignSelf='center' >
-          <Stack alignSelf='center'>
-              <Image
-                fill={true}
-                fit='contain'
-                src={`https://www.themoviedb.org/t/p/original/${mv.backdrop_path}`}
-              />
-            <Box
-              fill={true}
-              background={`radial-gradient(circle, rgba(255,246,246,0) 70%, ${col} 85%)`}
+    <>
+      {isLoading ? (
+        <Spinner size='large' />
+      ) : (
+        <Box
+          pad={{ horizontal: '100px' }}
+          background={{ color: 'accent-4' }}
+          direction='row'
+          justify='between'
+        >
+          <Box
+            style={{ position: 'sticky', top: 0 }}
+            height='medium'
+            width='medium'
+            direction='column'
+            background={{ color: 'accent-2' }}
+            margin={{ right: 'small' }}
+          >
+            <Button secondary alignSelf='start' icon={<LinkPrevious />} />
+            <Image
+              src={`https://www.themoviedb.org/t/p/original/${mvD.poster_path}`}
             />
-          </Stack>
-        </Box>
-      
-      </Box>
+            <RatingPanel
+              voteCount={mvD.vote_count}
+              voteScore={mvD.vote_average}
+            />
+          </Box>
 
-      <Box background='dark-2'>Footer</Box>
-    </Box>
+          <Box background={col}>
+            <Box width='xlarge'>
+              <BackdropImage backdropPath={mvD.backdrop_path} col={col} />
+            </Box>
+
+            <Box margin={{ bottom: '600px' }} pad={{ horizontal: 'large' }}>
+              <TitleText
+                title={mvD.title}
+                releaseDate={mvD.release_date}
+                director={dir}
+              />
+              <Box border='top' />
+              <Text
+                margin={{ vertical: 'small' }}
+                style={{ fontFamily: 'Mate SC' }}
+              >
+                {mvD.tagline?.toUpperCase()}
+              </Text>
+              <Text>{mvD.overview}</Text>
+              <Box
+                alignSelf='start'
+                direction='row'
+                pad={{ top: 'medium' }}
+                gap='small'
+              >
+                <AddToListButton listId='watchlist' movieId={props.id} />
+                <AddToListButton listId='watchedlist' movieId={props.id} />
+                <RatingBtn id={props.id} />
+              </Box>
+              <CastCrewDetailsTabs
+                cast={mvC.cast}
+                crew={mvC.crew}
+                details={details}
+              />
+            </Box>
+          </Box>
+        </Box>
+      )}
+    </>
   );
 };
-
-/*
-<Image
-              fit='cover'
-              // opacity='medium'
-              src={`https://www.themoviedb.org/t/p/original/${mv.backdrop_path}`}
-            />
-*/
 
 export default MovieDetails;
