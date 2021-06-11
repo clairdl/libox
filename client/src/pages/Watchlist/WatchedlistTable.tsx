8732ab823b7699c5d64f4fba6c3a817b2e28bedc
clairdl <52753311+clairdl@users.noreply.github.com>
2021-06-11 12:32:24 -0400

    remove cruft

--- client/src/pages/Watchlist/WatchedlistTable.tsx
+++ client/src/pages/Watchlist/WatchedlistTable.tsx
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { Box, DataTable } from 'grommet';
@@ -8,16 +5,6 @@ import { Box, DataTable } from 'grommet';
 import { columns } from './tableConfig';
 import { useAppSelector } from '../../hooks/reduxHooks';
 import { selectWatchlist } from '../../slices/list/listSlice';
-import { MovieListItem } from '../../slices/list/listTypes';
-
-// const SortableIcon = () => (
-//   <Blank color='text-xweak' opacity='0.3'>
-//     <g fill='none' stroke='#000' strokeWidth='2'>
-//       <path d='M 6 10 L 12 6 L 18 10' />
-//       <path d='M 6 14 L 12 18 L 18 14' />
-//     </g>
-//   </Blank>
-// );
 
 const WatchlistTable = () => {
   const history = useHistory();
